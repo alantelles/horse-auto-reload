b06148dadb17fadc6719f10a4e12d12ecbfa8555
Alan Telles <alantelles86@gmail.com>
2022-01-11 08:16:35 -0300

    fix: enable recursive directory detection

--- src/Horse.AutoReload.pas
+++ src/Horse.AutoReload.pas
@@ -24,46 +24,64 @@ var
   DetectChangeScript: string;
   LastModFile: longint = -1;
   FilesCount: longint = -1;
+  SessionCount: longint = 0;
   FirstScan: boolean = True;
 
 implementation
 
-function DetectChanges: boolean;
+
+function DetectChanges(ADir: string): boolean;
 var
   FileInfo: TSearchRec;
   HasChanges: boolean = False;
-  Count: integer = 0;
 begin
-  if FindFirst('*', faAnyFile, FileInfo) = 0 then
+  if FindFirst(ADir + directorySeparator + '*', faAnyFile, FileInfo) = 0 then
   begin
     Repeat
-      Count := Count + 1;
+      if (FileInfo.Name = '.') or (FileInfo.Name = '..') then
+        continue;
+      SessionCount := SessionCount + 1;
       if LastModFile < FileInfo.Time then
       begin
         HasChanges := True;
         LastModFile := FileInfo.Time;
       end;
+      if (FileInfo.Attr and faDirectory) = faDirectory then
+      begin
+        HasChanges := HasChanges or DetectChanges(ADir + DirectorySeparator + FileInfo.Name);
+      end;
     until FindNext(FileInfo) <> 0;
     FindClose(FileInfo);
-    if FilesCount <> Count then
-    begin
-      FilesCount := Count;
-      HasChanges := True;
-    end;
   end;
+
+  Result := HasChanges;
+end;
+
+function RunDetectChanges: boolean;
+var
+  HasChanges: boolean;
+begin
+  HasChanges := DetectChanges('.');
   if FirstScan then
     WriteLn(#$F0, #$9F, #$91, #$80,' Horse AutoReload: Filesystem was scanned. Respond a request with SendWithReloader function to reload page when detecting changes.');
   if HasChanges and not FirstScan then
     WriteLn(#$E2, #$9F, #$B3, ' Horse AutoReload: Filesystem of application containing folder has changed. Reload signal will be sent.');
   FirstScan := False;
+  if FilesCount <> SessionCount then
+  begin
+    FilesCount := SessionCount;
+    HasChanges := True;
+  end;
+  SessionCount := 0;
   Result := HasChanges;
 end;
+
 
 procedure ReportChanges(Req: THorseRequest; Res: THorseResponse; Next: {$IF DEFINED(FPC)}TNextProc{$ELSE}TProc{$ENDIF});
 var
   ResultStatus: integer = 200;
 begin
-  if DetectChanges then
+  if RunDetectChanges then
     ResultStatus := 204;
   Res.Status(ResultStatus);
 end;
@@ -77,7 +95,7 @@ function AutoReload(APort: integer): THorseCallback;
 var
   Fmt: string;
 begin
-  DetectChanges;
+  RunDetectChanges;
   THorse.Get('/__auto-reload-detect-changes', @ReportChanges);
   Fmt := '<script>setInterval(() =>  fetch("http://localhost:%d/__auto-reload-detect-changes").then(response => { if (response.status == 204) {console.log("Change detected. Reloading"); location.reload();} }),2000);</script>';
   DetectChangeScript := Format(Fmt, [APort]);
